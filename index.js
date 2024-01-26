dc44f1aa99326f2d13c91113d0defa2610b43574
abray <armando.ameri@studio.unibo.it>
2024-01-26 14:39:28 +0100

    filtering

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+console.log(characters);
 
+let pswLenght = 15
+const slideValue = document.querySelector(".sliderValue");
+const inputSlider = document.querySelector("input");
+inputSlider.oninput = () => {
+    let value = inputSlider.value;
+	slideValue.textContent = value;
+    pswLenght = inputSlider.value
+};
 
-
-const pswLenght = 15
-let psw1 = getRandomPsw()
-let psw2 = getRandomPsw()
-let psws = [psw1, psw2]
-console.log(psws)
+// let psw1 = getRandomPsw()
+// let psw2 = getRandomPsw()
+// let psws = [psw1, psw2]
+// console.log(psws)
 
 const buttonGenerator = document.getElementById('generator')
 const inputEl1 = document.getElementById('input-1')
 const inputEl2 = document.getElementById('input-2')
+const checkElABC = document.getElementById('uppercase')
+const checkElabc = document.getElementById('lowercase')
+const checkElnumber = document.getElementById('number')
+const checkElspecials = document.getElementById('specials')
 
 function getRandomPsw() {
-    let randomCharIndex = Math.floor( Math.random()*characters.length )
+    let filteredArray = ''
+    if(!checkElABC.checked && !checkElabc.checked && !checkElnumber.checked && !checkElspecials.checked){
+        alert("Please select at least a check!");
+        return ''
+    }
+    if (checkElABC.checked) {
+        filteredArray += characters.filter((char) => char.match(/[A-Z]/g)).join("")
+        console.log('ABC checked');
+    }
+    if (checkElabc.checked) {
+        filteredArray += characters.filter((char) => char.match(/[a-z]/g)).join("")
+        console.log('abc checked');
+    }
+    if (checkElnumber.checked) {
+        filteredArray += characters.filter((char) => char.match(/[0-9]+/)).join("")
+        console.log('123 checked');
+    }
+    if (checkElspecials.checked) {
+        filteredArray += characters.filter((char) => char.match(/[!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+/)).join("")
+        console.log('specials checked');
+    }
+    console.log(filteredArray.length);
+    console.log(filteredArray);
+    
     let psw = ''
+    let randomCharIndex = Math.floor( Math.random()*filteredArray.length )
     for (let i=0; i < pswLenght; i++ ) {
-        randomCharIndex = Math.floor( Math.random()*characters.length )
-        if(characters[randomCharIndex]==='undefined')
+        randomCharIndex = Math.floor( Math.random()*filteredArray.length )
+        if(filteredArray[randomCharIndex]==='undefined')
             console.log(randomCharIndex)
-        psw += characters[randomCharIndex]
+        psw += filteredArray[randomCharIndex]
     }
     return psw
 }
@@ -29,7 +64,8 @@ buttonGenerator.addEventListener('click', async () => {
     psw1 = getRandomPsw()
     psw2 = getRandomPsw()
     showPsw(psw1, psw2)
-    console.log('Generating...')
+    console.log('Generating password...')
+    console.log('Length: ' + pswLenght)
     console.log(psw1, psw2)
 })
 
@@ -40,13 +76,6 @@ function showPsw(psw1, psw2) {
 
 
 inputEl1.addEventListener('click', async (e) => {
-    /*console.log(navigator.clipboard);
-
-    await navigator.clipboard.writeText(inputEl1.innerText)
-    .then(() => {
-      console.log('The text has been copied');
-      alert("Copied the text: " + inputEl1.innerText);
-    });*/
     console.log(e.currentTarget.getAttribute("id"))
     console.log(e.target.innerText)
     
@@ -60,15 +89,6 @@ inputEl1.addEventListener('click', async (e) => {
     }
 })
 inputEl2.addEventListener('click', async (e) => {
-    /*
-    console.log(navigator.clipboard);
-
-    await navigator.clipboard.writeText(inputEl2.innerText)
-    .then(() => {
-      console.log('The text has been copied');
-      alert("Copied the text: " + inputEl2.innerText);
-    });*/
-    
     
     console.log(e.currentTarget.getAttribute("id"))
     console.log(e.target.innerText)
@@ -82,14 +102,6 @@ inputEl2.addEventListener('click', async (e) => {
         alert('Please hit "Generate password" button first')
     }
 })
-
-/*
-for (let i=0; i < characters.length; i++ ) {
-    console.log(characters[i])
-    if(characters[i]==='undefined')
-        console.log(i)
-}
-*/
 
 
 
